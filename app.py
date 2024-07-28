573cd456727d01b3848afc6106b8dc6993836158
suryalohit <51260818+suryalohit@users.noreply.github.com>
2024-07-28 06:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+import requests
+TOKEN = "6994416717:AAH_qEF1vSy1gZc1nXQ4eyM4dErJshFGJaM"
+chat_id = "998041732"
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    message="sel"
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+    requests.get(url)
     return render_template("index.html", title="Hello")
